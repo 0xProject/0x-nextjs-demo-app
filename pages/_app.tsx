abd1763f5d9ec74240a3b27ad65658a4b086fc0f
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-04-03 15:42:10 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -34,9 +34,7 @@ export default function App({ Component, pageProps }: AppProps) {
     >
       <WagmiConfig config={config}>
         <ConnectKitProvider>
-          <div className="connect-button-container">
-          <ConnectKitButton />
-            </div>
+        
           {mounted && <Component {...pageProps} />}
         </ConnectKitProvider>
       </WagmiConfig>
